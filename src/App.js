88220f2c462908418d00d998e0d3fc67ce832387
Daniel <dapascale@gmail.com>
2020-09-28 12:49:32 -0400

    hi

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   render(){
     return (
       <div className="App">
-        <h1>I have won React!</h1>
+        <h1>I have One React!</h1>
       </div>
     );
   }
